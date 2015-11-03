7138b8ea37e30f12df637e8346a0458893bebdfc
dlucas94 <darkogx@gmail.com>
2015-11-03 14:21:46 +0000

    plot dummy data

--- js/map.js
+++ js/map.js
@@ -9,7 +9,7 @@ ref.authWithPassword({
         console.log("Login Failed!", error);
     } else {
         console.log("Authenticated successfully with payload:", authData);
-        getEvents();
+//        getEvents();
     }
 });
 
@@ -18,31 +18,31 @@ function initMap() {
          zoom: 14,
          mapTypeId: google.maps.MapTypeId.ROADMAP
      };
-     var map = new google.maps.Map(document.getElementById('map'),
-     mapOptions);
+     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
      if (navigator.geolocation) {
          navigator.geolocation.getCurrentPosition(function (position) {
              initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
              map.setCenter(initialLocation);
+             getEvents(map);
          });
      }
 }
 
-function getEvents() {
-    //CONNECT TO FIREBASE
+function getEvents(map) {
     var ref = new Firebase("https://commonplaceapp.firebaseio.com/events")
-    //PULL EVENT DATA BY KEY
     ref.on('child_added', function(snapshot){
         var event = snapshot.val();
         console.log(event);
+        var lat = event.lat;
+        var lon = event.lon;
+        var coords = {lat: lat, lng: lon};
+        var name = event.name;
+        var eventMarker = new google.maps.Marker({
+            position: coords,
+            map: map,
+            title: name
+        });
+        eventMarker.setMap(map);
     })
-    //STORE INTO VARS
-    //plotEvents();
 }
-
-//function plotEvents() {
-//    //TAKE VARS
-//    //CREATE MARKER BASED ON VARS
-//    //CREATE INFOWINDOW FROM INFO
-//}
