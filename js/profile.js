c54eb48b5a5b3d446f145b492cdff2d1f4f7d3c6
dlucas94 <darkogx@gmail.com>
2015-11-09 16:58:54 +0000

    profile

--- js/profile.js
+++ js/profile.js
@@ -9,10 +9,9 @@ function getProfile() {
     ref.on("value", function(snapshot) {
         var newEvent = snapshot.val();
         var profilepic = newEvent.profilepic;
-        if (profilepic === undefined || null) {
+        if (profilepic === undefined || null) { //IF INVALID THEN SET TO PLACEHOLDER
             profilepic = "images/profileplaceholder.png";
         }
-        console.log(profilepic); //IF UNDEFINED OR NULL THEN USE PLACEHOLDER
         var fullname = newEvent.firstname + " " + newEvent.lastname;
         var location = newEvent.town + ", " + newEvent.country;
         var bio = newEvent.bio;
