cd7cc91ff4f472e34026e0c31604e3c13ed61c4a
dlucas94 <darkogx@gmail.com>
2015-11-09 15:52:30 +0000

    styling

--- js/profile.js
+++ js/profile.js
@@ -9,6 +9,14 @@ function getProfile() {
     ref.on("value", function(snapshot) {
         var newEvent = snapshot.val();
         var fullname = newEvent.firstname + " " + newEvent.lastname;
+        var location = newEvent.town + ", " + newEvent.country;
+        var bio = newEvent.bio;
+        var birthday = newEvent.birthday;
+        var interests = newEvent.interests;
         document.getElementById("fullname").innerHTML = fullname;
+        document.getElementById("location").innerHTML = location;
+        document.getElementById("bio").innerHTML = bio;
+        document.getElementById("birthday").innerHTML = birthday;
+        document.getElementById("interests").innerHTML = interests;
     })
 }
